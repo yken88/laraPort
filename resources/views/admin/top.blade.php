3794f6be10c8a114516d468c68ab4a30ec341e39
yken88 <yanokentarou@yanokentarounoMacBook-Air.local>
2020-10-30 18:21:28 +0900

    top にリンクを追加

--- resources/views/admin/top.blade.php
+++ resources/views/admin/top.blade.php
@@ -3,5 +3,6 @@
 @section('title', '管理者用ページ')
 
 @section('content')
-    
+    <a href="{{ route('admin.user.index') }}">ユーザ管理</a>
+    <a href="{{ route('admin.resident.index') }}">入居者管理</a>
 @endsection
