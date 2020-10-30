b87ccb1676428952fbe7351f53c4824fec623647
yken88 <yanokentarou@yanokentarounoMacBook-Air.local>
2020-10-30 18:51:36 +0900

    residentsテーブルにカラムを追加。

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,11 @@ Route::prefix('admin')->namespace('Admin')->as('admin.')->group(function(){
         //一度確認画面を開いた方がいいかもしれない。とりあえず作ってみるって感じで、少しずつ変えていく。
         Route::get('/{id}/delete', 'UserController@delete')->name('user.delete');
     });
-
+    
+    // 入居者管理
+    // Route::prefix('resident')->group(function(){
+    //     Route:get('/', 'ResidentController@index')->name('resident.controller');
+    // });
 });
 
         
