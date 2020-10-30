79dda2f99f1fe2b325feb922035e734b1989c5cf
yken88 <yanokentarou@yanokentarounoMacBook-Air.local>
2020-10-30 12:04:58 +0900

    AdminのみUserControlerを使えるように

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Admin;
 
 class UserController extends Controller
 {
+    public function construct()
+    {
+        $this->middleware('admin');
+    }
     //ユーザ一覧
     public function index()
     {
+        $admins = Admin::user();
         $users = User::orderBy('id', 'asc')->simplePaginate(5);
-        return view('user.index', compact('users'));
+        return view('user.index', compact('users', 'admins'));
     }
 
     public function create()
