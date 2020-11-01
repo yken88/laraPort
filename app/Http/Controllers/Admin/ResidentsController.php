1d29f0bf5953d2a3a2690e5368f199dee951e162
yken88 <yanokentarou@rrcs-172-254-99-23.nyc.biz.rr.com>
2020-11-01 22:47:50 +0900

    adlを簡単登録できるようにしたい。

--- app/Http/Controllers/Admin/ResidentsController.php
+++ app/Http/Controllers/Admin/ResidentsController.php
@@ -24,4 +24,30 @@ class ResidentsController extends Controller
         //dd($adl);
         return view('admin.residents.show_adl', compact('adl'));
     }
+
+    public function edit($id)
+    {
+        $adl = Adl::where('resident_id', $id)
+                ->with('resident')
+                ->get();
+
+        return view('admin.residents.edit', compact('adl'));
+    }
+
+    public function update(Request $request)
+    {
+        $adl = new Adl;
+        
+        $adl->移乗 = $request->移乗;
+        $adl->トイレ動作 = $request->トイレ動作;
+        $adl->平地歩行 = $request->平地歩行;
+        $adl->食事 = $request->食事;
+        $adl->排泄 = $request->排泄;
+        $adl->入浴 = $request->入浴;
+        $adl->更衣 = $request->更衣;
+        $adl->備考 = $request->備考;
+        $adl->save();
+
+        return redirect()->back();
+    }
 }
