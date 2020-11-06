ab730a8c1278cced88df01f7812851b7970421b0
yken88 <yanokentarou@yanokentarounoMacBook-Air.local>
2020-11-06 17:10:45 +0900

    adminのlogin(view)を見やすく変更。

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -12,22 +12,49 @@
     </ul>
 @endif
 
-<form method="post" class="form-group">
-    @csrf
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-    <ul>
-        <li>
-        <label>ログインID</label>
-        <input type="text" name="username" class="form-control" value="{{ old('username') }}">
-        </li>
-        <br>
-        <li>
-        <label>パスワード</label>
-        <input type="password" name="password" class="form-control" value="{{ old('username') }}">
-        </li>
-    </ul>
-    <div class="text-right">
-    <button type="sumbit" class="btn btn-primary">ログイン</button>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('admin.login') }}">
+                        @csrf
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">ユーザ名</label>
+
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="username">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</form>
+</div>
 @endsection
