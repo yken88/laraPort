173a9c933151f4fffa67715ba7f948ab4da756fd
yken88 <yanokentarou@rrcs-172-254-99-23.nyc.biz.rr.com>
2020-11-07 20:11:54 +0900

    adminのレイアウトの変更、それに伴う リンクを変更

---
+++ resources/views/layouts/admin_main.blade.php
@@ -0,0 +1,4 @@
+  <div class="album py-5 bg-secondary">
+    <div class="container">
+      @yield('content')
+  </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,24 +1,23 @@
-<nav class="site-header sticky-top py-1 bg-dark">
+<nav class="site-header sticky-top py-2 bg-dark">
   <div class="container d-flex flex-column flex-md-row justify-content-between">
-    <a class="py-2" href="#">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-    </a>
-    @if(Auth::check())
-    <a class="py-2 d-none d-md-inline-block text-light" href="{{ route('post.index') }}">申し送り一覧</a>  
-    @endif
+      <a href="#" class="navbar-brand d-flex align-items-center">
+      <i class="fa fa-book text-light mr-3" aria-hidden="true"></i>
+        <strong class="text-light">Send</strong>
+      </a>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-light py-2 d-none d-md-inline-block">Home</a>
-                        |
-                        <a href="{{ route('logout') }}" class="text-light py-2 d-none d-md-inline-block">ログアウト</a>
-
+                        <a href="{{ url('/home') }}" class="btn btn-outline-light mr-2">Home</a>
+                        
+                        <a href="{{ route('logout') }}" class="btn btn-outline-light mr-2">ログアウト</a>
+                        
+                        <a href="{{ route('post.index') }}" class="btn btn-outline-light">申し送り一覧</a>  
                     @else
-                        <a href="{{ route('admin.login') }}" class="py-2 d-none d-md-inline-block text-light" >管理者ログイン</a>  
-                        |
-                        <a href="{{ route('login') }}" class="text-light py-2 d-none d-md-inline-block">ログイン</a>
-                        |
-                        <a href="{{ route('register')}}"  class="text-light py-2 d-none d-md-inline-block">登録</a>
+                        <a href="{{ route('admin.login') }}" class="btn btn-outline-light mr-2" >管理者ログイン</a>  
+                        
+                        <a href="{{ route('login') }}" class="btn btn-outline-light mr-2">ログイン</a>
+                        
+                        <a href="{{ route('register')}}" class="btn btn-outline-light">登録</a>
                     @endauth
                 </div>
             @endif
