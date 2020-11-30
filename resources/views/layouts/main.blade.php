7afd4dbf54851b6121a3bd99218064d83c9fb588
yken88 <yanokentarou@yanokentarounoMacBook-Air.local>
2020-11-30 17:35:18 +0900

    welcome画面
    welcomにログインformを設置。見た目を整えた。
    またredirect先の変更。

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,11 +1,13 @@
+<main>
 <section class="jumbotron text-center">
     <div class="container">
       <h1 class="jumbotron-heading">@yield('title')</h1>
       <p class="lead text-muted">@yield('overview')</p>
     </div>
   </section>
-  <div class="bg-light p-5">
+  <div class="p-5 welcome">
     <div class="container">
       @yield('content')
     </div>
   </div>
+</main>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,20 +1,13 @@
 <!doctype html>
 <html lang="ja" >
-  <head>
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-    <link href="album.css" rel="stylesheet">
-    <link href="../example.css" rel="stylesheet">
-  </head>
+  @include('layouts.head')
   <body >
 
-<header>@include('layouts.header')</header>
+    @include('layouts.header')
 
-<main>@include('layouts.main')</main>
+    @include('layouts.main')
 
-<footer class="bg-dark">@include('layouts.footer')</footer>
+    @include('layouts.footer')
 
   </body>
 </html>
