b17ae06cccf33db8e1c3244b78a3d6c4349c4182
yken88 <yanokentarou@rrcs-172-254-99-23.nyc.biz.rr.com>
2020-12-03 22:12:54 +0900

    Add Procfile and set varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Fadades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
     }
 }
