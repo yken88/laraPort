52fa58f06751f427ec9faabd9a180e37b2b422d8
yken88 <65884316+yken88@users.noreply.github.com>
2020-12-08 17:52:50 +0900

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
 <div class="container">
     <p class="float-right">
-      <a href="/" class="text-light">トップに戻る</a>
+      <a href="/" class="text-dark">トップに戻る</a>
     </p>
   </div>
 </footer>
